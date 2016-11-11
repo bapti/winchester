14ddc75fadd886b55d996ccd47f796b892ae614f
Donald Robertson <donaldarobertson89@gmail.com>
2016-11-11 21:47:52 +0000

    First actual commit

---
+++ test/test_winchester.js
@@ -0,0 +1,10 @@
+var chai = require('chai')
+var should = chai.should()
+
+describe('winchester', function () {
+  it('should return true', function () {
+    var one = 1
+
+    return one.should.equal(1)
+  })
+})
